fe6f73fe0cacd28e40121923cbfa1f9521f07489
vengadeshpandiyan <75629406+vengadeshpandiyan@users.noreply.github.com>
2020-12-18 14:21:47 +0530

    Add files via upload

---
+++ build/precache-manifest.79d8ae9deed04b19b6e28cc6b82cd3b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67bee66079030d5200b05057477e925",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18d144e78445e25074e",
+    "url": "/static/css/main.cbf08a08.chunk.css"
+  },
+  {
+    "revision": "a38893909a20aa767ef6",
+    "url": "/static/js/2.efbbf1f6.chunk.js"
+  },
+  {
+    "revision": "a18d144e78445e25074e",
+    "url": "/static/js/main.d3b2b8c8.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  },
+  {
+    "revision": "1193c50a3862e1127caa43768cd71f8b",
+    "url": "/static/media/card-newone.1193c50a.jpg"
+  },
+  {
+    "revision": "2a8153ef719d8cb58b576262b1e181e3",
+    "url": "/static/media/hero-a.2a8153ef.svg"
+  },
+  {
+    "revision": "747791fbf2d6d54e92a0d5a652a18168",
+    "url": "/static/media/trees.747791fb.jpg"
+  }
+]);
